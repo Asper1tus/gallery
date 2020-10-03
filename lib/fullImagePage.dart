3ccfe4ca4066b69f75789eed3b80fb28f3232807
Asper1tus <dmitriy.timakhov@gmail.com>
2020-10-03 17:44:11 +0300

    Added loading progress in full image page

---
+++ lib/fullImagePage.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class FullImagePage extends StatelessWidget {
+  final title;
+  final fullUrl;
+
+  FullImagePage({this.title, this.fullUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: Image.network(fullUrl, loadingBuilder: (BuildContext context,
+              Widget child, ImageChunkEvent loadingProgress) {
+            if (loadingProgress == null) return child;
+            return Center(
+              child: CircularProgressIndicator(
+                value: loadingProgress.expectedTotalBytes != null
+                    ? loadingProgress.cumulativeBytesLoaded /
+                        loadingProgress.expectedTotalBytes
+                    : null,
+              ),
+            );
+          }),
+        ));
+  }
+}
